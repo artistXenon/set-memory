ef8e9dd5f6242eff62f1b0a2020fed33f3dd6bdf
artistXenon <xtramainpp@gmail.com>
2022-01-24 17:53:35 +0900

    [add] subscriber structure to store/regretting vuex

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,12 +1,28 @@
 import { reactive } from 'vue'
 
 const store = {
+    _subscriber: [
+      {
+        event: 'undefined',
+        cb: _ => {}
+      }
+    ],
+
     state: reactive({
       cases: []
     }),
   
     setCases(newCases) {  
       this.state.cases = newCases
+      this._subscriber.filter(({ event }) => evemt === 'cases').forEach(({ cb }) => cb())
+    },
+
+    setListener(event, callback) {
+      if (typeof event !== 'string' || typeof callback !== 'function') throw new Error('Illegal subscriber')
+      this._subscriber.push({
+        event,
+        cb: callback
+      })
     }
   }
 
