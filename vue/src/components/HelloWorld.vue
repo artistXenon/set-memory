0c21d40523faec9e19b96e06ca0a859f977317b6
Ailre <xtramainpp@gmail.com>
2022-02-03 21:10:37 +0900

    [init] case creation form to main page form div is reded for debug purpose

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
+    <h1>Set-memory application</h1>
+    <h3>Add case</h3>
+    <div class="create-case-form">
+      <form action="javascript:void(0);">
+        <div class="case-create-name">
+          <h5>Case name</h5> <input type="text" name="name" id="name"/>
+        </div>
+        <hr/>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'HelloWorld'
 }
 </script>
 
@@ -26,15 +24,40 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+h5 {
+  margin: 0.8rem auto;
+}
+hr {
+  border: none;
+  border-top: 1px solid #444;
+  margin: 0.9rem 5%;
+}
+input {
+  margin: 0;
+  padding: 0.2rem;
+  border-radius: 0.4rem;
+  border-width: 0.01rem;
+}
 ul {
   list-style-type: none;
   padding: 0;
 }
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 a {
   color: #42b983;
+}
+
+div.create-case-form {
+  padding-top: 0.1rem;
+  padding-bottom: 0.2rem;
+  margin: 0.7rem auto 0 auto;
+  max-width: 400px;
+  width: 90%;
+  background-color: #f00;
+}
+
+div.case-create-name * {
 }
 </style>
