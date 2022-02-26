d2bcf940a07959bcee4c99467262b80a4586fc4c
Ailre <xtramainpp@gmail.com>
2022-02-26 09:31:34 +0900

    reform set list to set table

--- vue/src/components/Sets.vue
+++ vue/src/components/Sets.vue
@@ -5,11 +5,22 @@
   
     <h2>Sets</h2>
     <div class="set-var">
-      <ul>
-        <li v-for="(item, index) in sets" v-bind:key="index">
-        {{item.elements}}
-        </li>
-      </ul>
+        <table>
+          <thead>
+            <tr>
+              <th v-for="(item, index) in setsDesc" v-bind:key="index">
+                {{item.name}}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, index) in sets" v-bind:key="index">
+              <td v-for="(it, id) in item.elements" v-bind:key="id">
+                {{it ? it : ""}}
+              </td>
+            </tr>
+          </tbody>
+        </table>
     </div>
   </div>
 </template>
@@ -21,7 +32,8 @@ export default {
   name: 'Sets',
   data() {
     return {
-      sets: []
+      sets: [],
+      setsDesc: []
     }
   },
   mounted() {
@@ -38,6 +50,7 @@ export default {
       const caseId = Number(this.$route.params.caseId)
       const c = this.$store.getCase(caseId)
       if (c) {
+        this.setsDesc = JSON.parse(c.set_json)
         try {
           const r = await axios.get(this.$api_domain + `/api/cases/${caseId}/sets`)
           const { result } = r.data
